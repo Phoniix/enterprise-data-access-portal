220b3d8dfb21ef50d5e5e21e421244379c35c7b0
Phoniix <seanasb52058@gmail.com>
2025-07-30 11:31:39 -0500

    Initial Commit

---
+++ EnterpriseDataAccessPortal/src/main/java/com/maxpotential/EnterpriseDataAccessPortal/EnterpriseDataAccessPortalApplication.java
@@ -0,0 +1,13 @@
+package com.maxpotential.EnterpriseDataAccessPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseDataAccessPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseDataAccessPortalApplication.class, args);
+	}
+
+}
---
+++ EnterpriseDataAccessPortal/src/test/java/com/maxpotential/EnterpriseDataAccessPortal/EnterpriseDataAccessPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxpotential.EnterpriseDataAccessPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseDataAccessPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
