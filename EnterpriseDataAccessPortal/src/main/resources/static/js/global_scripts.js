c51b8e07db870e6ab38171c9e60027cd66c89a98
Phoniix <seanasb52058@gmail.com>
2025-08-01 08:59:36 -0500

    Started on home page, added functionality to show/hide sections when clicked on the navbar. Just started on Homepage

---
+++ EnterpriseDataAccessPortal/src/main/resources/static/js/global_scripts.js
@@ -0,0 +1,36 @@
+
+// Show/Hide Sections (Single Page Application [SPA] Funtionality)
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.spa-section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    function showSectionByHash(hash) {
+        sections.forEach(section => {
+            section.classList.add('hidden');
+        });
+
+        const target = document.querySelector(hash);
+        if(target) target.classList.remove('hidden');
+    }
+
+    // Click Navigation MGMT
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const hash = link.getAttribute('href');
+            history.pushState(null, '', hash); // update URL live. No reload necesary.
+            showSectionByHash(hash);
+        });
+    });
+
+    // Handle back/forth navigation
+    window.addEventListener('popstate', () => {
+        showSectionByHash(window.location.hash || '#home');
+    });
+
+    // Prevent auto scroll on section load
+    
+
+    // Load home page on initial page load
+    showSectionByHash(window.location.hash || '#home');
+});
